3d034ac6f6a743b2918437888c8f02b6dd1c2782
ltaoo <litaowork@aliyun.com>
2017-11-25 11:45:15 +0800

    【实现】向数据库插入数据

--- src/main/kotlin/com/example/koltinDemo/Controller.kt
+++ src/main/kotlin/com/example/koltinDemo/Controller.kt
@@ -1,17 +1,27 @@
 package com.example.koltinDemo
 
+import com.example.koltinDemo.Entity.Customer
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 /**
  * Created by ltaoo on 2017/11/25.
  */
 @RestController
-class CustomerController @Autowired constructor(val repository: CustomerRepository) {
+class CustomerController
+    @Autowired constructor(val repository: CustomerRepository) {
+
     @RequestMapping("/")
     fun findAll() = repository.findAll()
-    @RequestMapping("/{name}")
+
+    @RequestMapping("/{name}", method = arrayOf(RequestMethod.GET))
     fun findByLastName(@PathVariable name: String) = repository.findByLastName(name)
+
+    @RequestMapping("/create")
+    @ResponseBody
+    fun create(firstname: String?, lastname: String?) {
+        print(firstname);
+        var newUser = Customer("li", "tao")
+        repository.save(newUser)
+    }
 }
--- src/main/kotlin/com/example/koltinDemo/Entity/Customer.kt
+++ src/main/kotlin/com/example/koltinDemo/Entity/Customer.kt
@@ -1,17 +1,17 @@
 package com.example.koltinDemo.Entity
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 /**
  * Created by ltaoo on 2017/11/25.
  */
 @Entity
+@Table(name = "user")
 data class Customer(
         var firstName: String = "",
         var lastName: String = "",
-        @Id @GeneratedValue(strategy = GenerationType.AUTO)
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
         var id: Long = 0
 )
